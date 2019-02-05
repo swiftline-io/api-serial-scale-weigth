4bf0a135ed6b1ec006be0bb7e2e118652dc1fb69
lmdf77 <luismiguel.ryan@gmail.com>
2019-02-05 18:47:18 -0400

    improvement in the structured view of the results

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 let _ = require('underscore');
 let scale = require('./example');
 
-let status_s = scale.status.kg.slice(1, 4);
-let status_u = scale.status.kg.slice(4, 8);
+let status_s = scale.status.lb1.slice(1, 4);
+let status_u = scale.status.lb1.slice(4, 8);
 let weigth = scale.weigth;
 
 data(status_s, status_u, weigth);
@@ -14,18 +14,27 @@ function data( S, U, W) {
     if( _.isEqual([0, 1, 0], S) ) {
       let int = W.slice(0, 3).join('');
       let float = W.slice(3, 5).join('');  
-      return result = parseInt(int) + '.' + parseInt(float) + ' Kg';
+      return result = {
+        'status': "Good",
+        'weigth': parseInt(int) + '.' + parseInt(float) + ' Kg'
+      };
     }
-
+  
     if( _.isEqual([1, 0, 0], S) ) {
-      return result = parseInt(W.join('')) + ' g';
+      return result = {
+        'status': "Good",
+        'weigth': parseInt(W.join('')) + ' g' 
+      };
     }
 
     if( _.isEqual([0, 1, 1], S) ) {
       let lb = W.slice(0, 2).join('');
       let oz = W.slice(3, 5).join('');
       let float = W.slice(4).join('');
-      return result = parseInt(lb) + ' lb, ' + parseInt(oz) + '.' + parseInt(float) + ' oz';
+      return result = {
+        'status': "Good",
+        'weigth': parseInt(lb) + ' lb, ' + parseInt(oz) + '.' + parseInt(float) + ' oz'
+      };
     }
 
     if( _.isEqual([1, 0, 1], S) ) {
@@ -33,52 +42,88 @@ function data( S, U, W) {
       if( c[4] === 0 ) {
         let lb = W.slice(0, 2).join('');
         let oz = W.slice(2, 4).join('');
-        return result = parseInt(lb) + ' lb, ' + parseInt(oz) + ' oz';
+        return result = {
+          'status': "Good",
+          'weigth': parseInt(lb) + ' lb, ' + parseInt(oz) + ' oz' 
+        };
       }
 
       if( c[4] === 1 ) {
         let lb = W.slice(0, 2).join('');
         let oz = W.slice(2, 4).join('');
-        return result = parseInt(lb) + ' lb, ' + parseInt(oz) + '-1/4 oz';
+        return result = {
+          'status': "Good",
+          'weigth': parseInt(lb) + ' lb, ' + parseInt(oz) + '-1/4 oz' 
+        };
       }
 
       if( c[4] === 2 ) {
         let lb = W.slice(0, 2).join('');
         let oz = W.slice(2, 4).join('');
-        return result = parseInt(lb) + ' lb, ' + parseInt(oz) + '-1/2 oz';
+        return result = {
+          'status': "Good",
+          'weigth': parseInt(lb) + ' lb, ' + parseInt(oz) + '-1/2 oz' 
+        };
       }
 
       if( c[4] === 3 ) {
         let lb = W.slice(0, 2).join('');
         let oz = W.slice(2, 4).join('');
-        return result = parseInt(lb) + ' lb, ' + parseInt(oz) + '-1/3 oz';
+        return result = {
+          'status': "Good",
+          'weigth': parseInt(lb) + ' lb, ' + parseInt(oz) + '-1/3 oz' 
+        };
       }
     }
   }
-
+  
   if( _.isEqual([0, 0, 1, 1], U) ) {
-    return result = '"tArE"';
+    return result = {
+      'status': "Bad",
+      'value': "tArE" 
+    };
   }
   if( _.isEqual([0, 1, 0, 0], U) ) {
-    return result = '"Lo"';
+    return result = {
+      'status': "Bad",
+      'value': "Lo" 
+    };
   }
   if( _.isEqual([0, 1, 0, 1], U) ) {
-    return result = '"Err"';
+    return result = {
+      'status': "Bad",
+      'value': "Err" 
+    };
   }
   if( _.isEqual([0, 1, 1, 0], U) ) {
-    return result = '"ErrL"';
+    return result = {
+      'status': "Bad",
+      'value': "ErrL" 
+    };
   } 
   if( _.isEqual([0, 1, 1, 1], U) ) {
-    return result = '"----"';
+    return result = {
+      'status': "Bad",
+      'value': "----"
+    };
   }
   if( _.isEqual([1, 1, 0, 0], U) ) {
-    return result = '"8888"';
+    return result = {
+      'status': "Bad",
+      'value': "8888"
+    };
   }
   if( _.isEqual([1, 1, 0, 1], U) ) {
-    return result = '"Err"';
+    return result = {
+      'status': "Bad",
+      'value': "Err"
+    };
   }
   if( _.isEqual([1, 1, 1, 1], U) ) {
-    return result = '"CAL"';
+    return result = {
+      'status': "Bad",
+      'value': "CAL"
+    };
   }
 
 }
